17e350a2089b9014d2c10d4d5aee079954798df7
altradCS <145883008+altradCS@users.noreply.github.com>
2024-02-06 14:38:51 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,9 @@
 mkdir -p ~/.streamlit/
 
-echo "\
-[general]\n\
-email = \"your-lah.net9@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS=false\n\
-headless = true\n\
-\n\
+echo "
+[server]\\n
+headless = true\\n
+enableCORS=false\\n
+port = $PORT\\n
+\\n
 " > ~/.streamlit/config.toml
